8ecf35cb9a3e37017ce5a198dfa36a1ee840eaf2
Saurabh Naik <naiksaurabhd@gmail.com>
2024-05-13 21:38:03 +0530

    bug resolved

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages,setup
 
 setup(
     name='mcqgenrator',
-    version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    version='0.0.2',
+    author='saurabh naik',
+    author_email='naiksaurabhd@gmail.com',
+    install_requires=["langchain","streamlit","python-dotenv","PyPDF2","google-generativeai"],
     packages=find_packages()
 )
